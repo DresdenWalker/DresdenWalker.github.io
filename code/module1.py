7bfa0151b6d4deb7d0e0973b07fdc68ffe436ded
DresdenWalker <camdenchasse@icloud.com>
2017-01-27 10:12:16 -0500

    Update module1.py

--- code/module1.py
+++ code/module1.py
@@ -1,5 +1,5 @@
 #PYTHON2 CODE
-print 'Hola senor, es el profesor enseña? Sí senor.'
+print 'Hola senor, es el profesor enseñando? Sí senor.'
 
 #PYTHON3 CODE
-# print('Hola senor, es el profesor enseña? Sí senor.')
+# print('Hola senor, es el profesor enseñando? Sí senor.')
