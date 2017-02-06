976fb20fee1067336900ca16f95443876ca3023d
DresdenWalker <camdenchasse@icloud.com>
2017-02-06 10:27:00 -0500

    Update module1.py

--- code/module1.py
+++ code/module1.py
@@ -1,5 +1,12 @@
-#PYTHON2 CODE
-print 'Hola senor, es el profesor enseñando? Sí senor.'
+# BASH COMMAND man
 
-#PYTHON3 CODE
-# print('Hola senor, es el profesor enseñando? Sí senor.')
+man man 
+
+# This command opens up the manuel for bash which can show you many commands and teaches you what each one does, alhough it is 
+# a bit difficult to understand withought a understanding of basic bash
+#BASH COMMAND man apropos
+
+man apropos
+
+# This opens up a section of the manuel that shows information on the command apropsos which is a alis of sorts, taking a shorter
+# word or command and displays the same result as if you'd entered the shortened command.
