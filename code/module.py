cd203cadcff79ba87fb13c28456ee88e12473aa4
DresdenWalker <camdenchasse@icloud.com>
2017-03-07 09:46:24 -0500

    Create module.py

---
+++ code/module.py
@@ -0,0 +1,13 @@
+BASH COMMAND #
+
+#This is a comment and doesn't effect code.
+
+BASH QUOTATION COMMANDS 
+
+echo "I am $LOGNAME"
+
+#This will pring out the log-in name of the computers host.
+
+echo 'I am $LOGNAME'
+  
+#This will print out what is says, having no change to the words.
