26a43952ab183de24a9e7d8473911c0c1a0f4ae9
DresdenWalker <camdenchasse@icloud.com>
2017-04-10 10:03:19 -0400

    Create canvas2d.js

---
+++ code/canvas2d.js
@@ -0,0 +1,10 @@
+c = document.createElement('canvas');
+c.width = 800;
+c.height=600;
+document.body.appendChild(c);
+ctx = c.getContext('2d');
+ctx.beginPath;
+ctx.beginPath();
+ctx.fillRect(0,0,800,600);
+ctx.fillStyle = 'green';
+ctx.fillRect(0,0,700,700);
