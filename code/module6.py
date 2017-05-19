b3416280960c3a7e8ae8e67afeddbc6b06af727f
DresdenWalker <camdenchasse@icloud.com>
2017-05-19 09:48:09 -0400

    Update module6.py

--- code/module6.py
+++ code/module6.py
@@ -1,1 +1,5 @@
-
+#This first part I use the command to create names.
+names=("bob, peter, jimbo and $USER") 
+#This sets these names equal to "names" 
+echo $names 
+#This will then show what the "names" file is labeled as
